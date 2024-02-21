6a276561f783a4ad10b3b732286956346a07d6b8
DESKTOP-HMKBFVT\USER <radtani8810@gmail.com>
2024-02-21 14:25:06 +0900

    make dice

--- java/A問題/ABC/src/App.java
+++ java/A問題/ABC/src/App.java
@@ -1,5 +1,10 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int dice =0;
+        while(dice<6){
+            dice = 1 + (int)(Math.random()*6.0);
+            System.out.println("6以外が出ていないので継続");
+        }
+        System.out.println("6が出たので処理を終了します");
     }
 }
